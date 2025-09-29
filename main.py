e385ccf8e6d7ac257e9f4aca8c98503426a0ab76
dnzk <danishkalesaran@gmail.com>
2025-09-29 02:03:45 +0000

    feat: set fps

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
+    clock = pygame.time.Clock()
 
     while True:
         for event in pygame.event.get():
@@ -13,6 +15,8 @@ def main():
 
         screen.fill(pygame.Color(0, 0, 0))
         pygame.display.flip()
+        t = clock.tick(60)
+        dt = t / 1000
 
 
 if __name__ == "__main__":
